629169bf748e3e686abef56105f2a032083b77ab
LesBoys <lesboys@0.0>
2025-08-02 23:41:03 +0000

    +Action: Edit+ ensure ver process version as string

--- ver.c
+++ ver.c
@@ -3,6 +3,6 @@
 #define g_printf printf // g_printf因未知原因无法使用
 
 int main() {
-    g_printf("libeventemitter %f\n", VER);
+    g_printf("libeventemitter %s\n", VER);
     g_printf("This is a free software under BSD-3\n");
 }
