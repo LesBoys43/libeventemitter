d2f2ba5b9aa9bde1260b87f29d37870b584c2595
LesBoys <lesboys@0.0>
2025-08-03 01:02:03 +0000

    +Action: Edit+ fix marco expand bug (try #3)

--- ver.c
+++ ver.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 #define g_printf printf // g_printf因未知原因无法使用
+#define REALVER #VER
 
 int main() {
-    g_printf("libeventemitter %s\n", VER);
+    g_printf("libeventemitter %s\n", REALVER);
     g_printf("This is a free software under BSD-3\n");
 }
